844bc4567172f758831f191be0ebe526792ea064
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-08-25 21:16:35 +0900

    feat: add surface

--- app/src/main/java/com/masahikokobayashi/smartcontractlab/MainActivity.kt
+++ app/src/main/java/com/masahikokobayashi/smartcontractlab/MainActivity.kt
@@ -3,10 +3,14 @@ package com.masahikokobayashi.smartcontractlab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +25,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
